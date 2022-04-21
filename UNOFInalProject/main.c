7e6e423203c41af8232a4721f3d4b5103cb98123
Evan Martin <ewmartin@email.arizona.edu>
2022-04-21 15:50:29 -0700

    Initial Commit

---
+++ UNOFInalProject/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  UNOFInalProject
+//
+//  Created by Evan Martin on 4/21/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
