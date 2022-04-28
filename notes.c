146a28e6d8b06b7eb15a73a143911175e927fdb6
Evan Martin <ewmartin@email.arizona.edu>
2022-04-28 12:35:06 -0700

    6

---
+++ notes.c
@@ -0,0 +1,25 @@
+//
+//  notes.c
+//  UNOFInalProject
+//
+//  Created by Evan Martin on 4/28/22.
+//
+
+/*
+ typedef struct person_s {
+    char name[];
+    int age;
+    person.s *prev, *next;  //pointing to the previous and next person
+ } person;
+ 
+ void addMember()
+ {
+ 
+ }
+ 
+ 
+ 
+ */
+
+
+#include <stdio.h>
